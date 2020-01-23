2ae88a5f70b0d11daf5a5009e4c02321b7bd6e1b
Layon Martins Fonseca <layonmartins03@gmail.com>
2020-01-22 21:14:03 -0300

    add clicklistener to listen for clicks on listDrinks

--- app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -6,6 +6,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.widget.AdapterView;
 
 public class DrinkCategoryActivity extends Activity {
 
@@ -13,11 +16,30 @@ public class DrinkCategoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+
         ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 Drink.drinks);
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
+
+        //Create a listener to listen for clicks
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        //Pass the drink the user clicks on to DrinkActivity
+                        Intent intent = new Intent(DrinkCategoryActivity.this,
+                                                                      DrinkActivity.class);
+                        intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                        startActivity(intent);
+                    }
+                };
+
+        //Assign the listener to the list view
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
